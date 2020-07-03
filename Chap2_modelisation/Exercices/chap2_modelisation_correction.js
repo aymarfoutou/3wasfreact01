5cbf59067b54142e10920cf621ec286d15a8d820
Antoine07 <antoine.lucsko@gmail.com>
2020-07-03 15:47:18 +0200

    exercice 1

--- Chap2_modelisation/Exercices/chap2_modelisation_correction.js
+++ Chap2_modelisation/Exercices/chap2_modelisation_correction.js
@@ -32,3 +32,40 @@ db.books.insertOne({ title: "JS", category_id: programmation._id });
 db.books.insertOne({ title: "PosgreSQL", category_id: SQL._id });
 db.books.insertOne({ title: "MySQL", category_id: SQL._id });
 db.books.insertOne({ title: "MongoDB", category_id: NoSQL._id });
+
+
+// 2. Puis faites une requête pour récupérer les livres dans la catégorie programmation.
+
+db.books.find({ category_id: programmation._id })
+
+// 3. Combien de livre y a t il dans la catégorie NoSQL ? 
+
+db.books.find( {category_id: NoSQL._id } ).count()
+
+// 4.
+
+const newBooks = [
+    { title : "Python & SQL"}, // Programmation & SQL
+    { title : "JS, SQL ou NoSQL" }, // Programmation NoSQL SQL
+    {title : "Pandas & SQL & NoSQL"}, // SQL, NoSQL et Programmation
+    { title : "Modélisation des données"} // aucune catégorie
+]
+
+db.books.insertOne({ title: "Python & SQL", category_id: [SQL._id, programmation._id] });
+db.books.insertOne({ title: "JS, SQL ou NoSQL", category_id: [SQL._id, programmation._id, NoSQL._id] });
+db.books.insertOne({ title: "Pandas & SQL & NoSQL", category_id: [SQL._id, programmation._id, NoSQL._id] });
+db.books.insertOne({ title: "Modélisation des données"});
+
+// ET si votre document est vraiment ...
+db.books.insertOne({ title: "Normalisation", category_id: null });
+db.books.insertOne({ title: "Conference CV", category_id: [] });
+db.books.insertOne({ title: "Planning 2/2", category_id: 0});
+db.books.insertOne({ title: "Planning 1/2", category_id: ""});
+
+// On peut faire une requête qui vérifie tous les cas probable 
+db.books.find(  { $or : [  
+        { category_id : { $size : 0  } }, 
+        { category_id : { $exists : false }},
+        { category_id : { $in : [ null, "", 0 ]} }
+    ] 
+}, { _id : 0} )
