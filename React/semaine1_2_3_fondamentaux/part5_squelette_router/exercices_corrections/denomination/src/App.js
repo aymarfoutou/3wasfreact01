ac0e80779f83963a89695530dbf78c8cfd0c175b
Antoine07 <antoine.lucsko@gmail.com>
2020-08-18 11:37:12 +0200

    denomination

---
+++ React/semaine1_2_3_fondamentaux/part5_squelette_router/exercices_corrections/denomination/src/App.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component{
+
+  render(){
+
+    return (
+      < >
+        <h1>Dénomination</h1>
+        <Denomination />
+      </>
+    )
+  }
+}
+export default App;
