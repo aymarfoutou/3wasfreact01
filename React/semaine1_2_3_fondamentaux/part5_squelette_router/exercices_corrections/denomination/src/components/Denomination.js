67788b3e7313b14bef7c93d79a5c5f8b8416517b
Antoine07 <antoine.lucsko@gmail.com>
2020-08-18 11:58:15 +0200

    tokens show

--- React/semaine1_2_3_fondamentaux/part5_squelette_router/exercices_corrections/denomination/src/components/Denomination.js
+++ React/semaine1_2_3_fondamentaux/part5_squelette_router/exercices_corrections/denomination/src/components/Denomination.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 
-class Denomination extends Component{
+const denomination = [1, 5, 10, 20, 50, 100, 200]
 
-  render(){
+class Denomination extends Component {
 
-    return (
-     <div >
-         Hello denomination
-     </div>
-    )
-  }
+    render() {
+
+        return (
+            <div >
+                <ul>
+                    <h2>Liste de token disponibles</h2>
+                    {denomination.map((token, i) =>
+                        <li key={i} >{token}</li>
+                    )}
+                </ul>
+                <form>
+                    TODO faire la sais utilisateur avec state
+                </form>
+            </div>
+        )
+    }
 }
 
 // export du composant
